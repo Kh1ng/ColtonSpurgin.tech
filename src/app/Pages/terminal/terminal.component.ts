805bbce00b940a6f884c2595bc32ce9141fc907c
Khing <khing@pm.me>
2023-02-17 19:35:04 -0600

    terminal testing temp removed

--- src/app/Pages/terminal/terminal.component.ts
+++ src/app/Pages/terminal/terminal.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { TheRearService } from './the-rear.service';
 
 @Component({
   selector: 'app-terminal',
   templateUrl: './terminal.component.html',
   styleUrls: ['./terminal.component.scss'],
+  providers: [TheRearService],
 })
 export class TerminalComponent implements OnInit {
-
-
   constructor() {
     // screen = this.getStart();
   }
