7ac5a16ebfafb695b24a4806b37c14146d0f68dc
Colton <khing@pm.me>
2023-04-06 20:55:42 -0500

    build and deploy, responsive styles

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,10 @@ function App() {
           width={50}
         ></img>
       </div>
-      <div className="w-fit inline-block mx-auto mt-32 sm:mt-0 sm:p-8">
+      <div className="w-fit flex md:mx-auto p-1 max-w-md flex-col gap-1">
         <div className="inline-block align-text-top text-left pb-2 text-sm">
-          <span className="text-200">Hello, I am </span>
-          <span className="text-4xl font-bold font-serif text-50">
+          <span className="text-200 sm:px-1">Hello, I am </span>
+          <span className="text-4xl font-bold font-serif text-50 sm:px-1">
             Colton Spurgin
           </span>
           <span className="text-200 font-sans">
@@ -30,14 +30,14 @@ function App() {
             and I like to build things.
           </span>
         </div>
-        <div className="max-w-md">
+        <div className="max-w-md sm:px-1">
           <p className="text-left text-900 pt-2 pb-3 font-sans">
             I am currently in search of my next challenge. Among my many
             interests, I am currently most interested in full stack web
             development.
           </p>
         </div>
-        <div className="flex pt-3">
+        <div className="flex pt-3 sm:px-1">
           <a href="https://bork.coltonspurgin.tech/">
             {/* button sucks */}
             <button className="pt-3 btn bg-200 hover:bg-400 text-900 font-bold py-2 px-4 rounded">
@@ -46,19 +46,19 @@ function App() {
           </a>
         </div>
       </div>
-      <div className="absolute flex bottom-0 align-baseline p-1 w-screen">
-        <div className="flex lg:flex-col sm:align-middle sm:justify-center">
-          <div className="flex">
+      <div className="absolute flex bottom-0 align-baseline p-1 w-screen sm:pb-6">
+        <div className="flex lg:flex-col sm:align-middle sm:justify-center row-span-full sm:grid sm:grid-rows-3 w-screen md:justify-start">
+          <div className="flex mx-auto">
             <a href="https://github.com/Kh1ng">
               <FaGithub size={42} />
             </a>
           </div>
-          <div className="flex">
+          <div className="flex mx-auto">
             <a href="https://www.linkedin.com/in/coltonspurgin">
               <FaLinkedin size={42} />
             </a>
           </div>
-          <div className="flex">
+          <div className="flex mx-auto">
             <a href="mailto:colton.spurgin1@gmail.com">
               <FaEnvelope size={42} />
             </a>
