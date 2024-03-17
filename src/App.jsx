5b78c5155ccc888cf0683a32b843d8cf4779e1c3
Colton Spurgin <khing@pm.me>
2024-03-17 10:15:22 -0500

    router and query added

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import Welcome from "./components/Welcome";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
 function App() {
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-      </Routes>
-    </BrowserRouter>
-  </QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
