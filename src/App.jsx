c0e9c2424ed4ec4425d0e495e7bac16de86b16cb
Colton Spurgin <khing@pm.me>
2024-03-17 09:52:55 -0500

    browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import Welcome from "./components/Welcome";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
-  return <Welcome />;
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+      </Routes>
+    </BrowserRouter>
+  </QueryClientProvider>;
 }
 
 export default App;
